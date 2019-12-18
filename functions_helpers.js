068bb397f043b51421dfe63b88ed732dfcff280e
Pedro Henrique <38913462+PH7-Jack@users.noreply.github.com>
2019-12-18 08:51:38 -0300

    Create functions_helpers.js

---
+++ functions_helpers.js
@@ -0,0 +1,10 @@
+ //array helpers
+    //get the last item of array
+    $lastItemArray(array) {
+      return array[array.length - 1];
+    }
+    
+    //perfect clone your array or object (this method dont extend your object)
+    $cloneValue(value) {
+      return JSON.parse(JSON.stringify(value));
+    }
